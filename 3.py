a4af3dc9f99cdf985aa726ffc228bfe13350794f
Sebastian Tobie <stop5@users.noreply.github.com>
2017-12-06 01:41:15 +0100

    solved day 3

--- 3.py
+++ 3.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
 
 
+def deliver(directions, robo=False):
+    santapos = [0, 0]
+    robopos = santapos[:]
+    houses = [tuple(santapos), ]
+    santa = True
+    for direction in directions:
+        if robo:
+            if santa:
+                pos = santapos
+            else:
+                pos = robopos
+            santa = not santa
+        else:
+            pos = santapos
+        if direction == 60:
+            pos[0] -= 1
+        elif direction == 62:
+            pos[0] += 1
+        elif direction == 94:
+            pos[1] += 1
+        elif direction == 118:
+            pos[1] -= 1
+        if tuple(pos) not in houses:
+            houses.append(tuple(pos))
+    return len(houses)
+
+
 if __name__ == "__main__":
-    with open("") as dfile:
+    with open("3", "br") as dfile:
         data = dfile.read()
-    
+    print("Solution 1: ", deliver(data))
+    print("Solution 1: ", deliver(data, True))
