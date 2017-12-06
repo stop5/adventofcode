d680dfa0ff71438215618e4b2f17754a32e394c3
Sebastian Tobie <stop5@users.noreply.github.com>
2017-12-06 01:13:01 +0100

    solved day 2

--- 2.py
+++ 2.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 
 
+def calculate_surface(h, w, l):
+    f = h * w
+    r = h * l
+    t = w * l
+    return min(f, r, t) + (f + r + t) * 2
+
+
+def calculate_bow(h, w, l):
+    f = h * w * 2
+    r = h * l * 2
+    return h * w * l + min(h + l, h + w, w + l) * 2
+
+
+def traverse(data, computer):
+    surface = 0
+    for line in data:
+        line = [int(a) for a in line.split("x")]
+        surface += computer(*line)
+    return surface
+
+
 if __name__ == "__main__":
-    with open("") as dfile:
-        data = dfile.read()
-    
+    with open("2") as dfile:
+        data = dfile.readlines()
+    print("Solution 1: ", traverse(data, calculate_surface))
+    print("Solution 2: ", traverse(data, calculate_bow))
